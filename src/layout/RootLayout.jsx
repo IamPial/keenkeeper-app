71a0ad86c96f491fd950fa9e2c766ec24ae2e2df
Pial <pialname394@gmail.com>
2026-04-14 03:46:52 +0600

    imports outlet from react router

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
     <div>
       <h2>RootLayout</h2>
+      <Outlet />
     </div>
   );
 };

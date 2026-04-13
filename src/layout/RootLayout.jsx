764afe8704b39d4003f5272c978e04b95cdd5a19
Pial <pialname394@gmail.com>
2026-04-14 04:31:56 +0600

    rendering Navbar components into the RootLayout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Navbar from "../components/shared/Navbar";
 
 const RootLayout = () => {
   return (
     <div>
-      <h2>RootLayout</h2>
+      <Navbar />
       <Outlet />
     </div>
   );

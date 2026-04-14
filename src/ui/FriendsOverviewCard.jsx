ddc839e4c2b52ad4bf34b8cf34ec156a03ee8563
Pial <pialname394@gmail.com>
2026-04-14 14:46:32 +0600

    create friends overview card component for reusable components

---
+++ src/ui/FriendsOverviewCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FriendsOverviewCard = ({ value, text }) => {
+  return (
+    <div className=" rounded-lg text-center p-8 space-y-2 shadow-sm">
+      <h2 className="font-semibold text-[#244D3F] text-[32px]">{value}</h2>
+      <h2 className="text-lg text-slate-500 ">{text}</h2>
+    </div>
+  );
+};
+
+export default FriendsOverviewCard;

01553903d83826bb39d0493309ffd1957bb5ba46
Pial <pialname394@gmail.com>
2026-04-15 02:08:51 +0600

    created timeline pages listItem markup

--- src/pages/timeline/TimeLinePage.jsx
+++ src/pages/timeline/TimeLinePage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import callImg from "../../assets/call.png";
+import textImg from "../../assets/text.png";
+import videoImg from "../../assets/video.png";
 
 const TimeLinePage = () => {
   return (
-    <div className="py-20 container mx-auto">
-      <div className="space-y-6">
+    <div className="py-20 container mx-auto px-2 md:px-0">
+      <div className="space-y-6 mb-6">
         <h2 className="text-5xl font-bold text-slate-900">Timeline</h2>
         <div className="dropdown dropdown-center  ">
           <div
@@ -22,6 +25,21 @@ const TimeLinePage = () => {
             <li>Hello</li>
             <li>Hello</li>
           </ul>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 gap-6 ">
+        <div className="flex gap-6 items-center border border-gray-200 rounded-lg p-4 ">
+          <img src={videoImg} alt="video" />
+          <div>
+            <h2 className="font-medium text-2xl text-[#244D3F]">
+              Video{" "}
+              <span className="text-lg text-gray-500 font-normal">
+                with Tom Baker
+              </span>
+            </h2>
+            <p className="font-medium text-gray-500">March 29, 2026</p>
+          </div>
         </div>
       </div>
     </div>

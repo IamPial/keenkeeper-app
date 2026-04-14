56c76d2ca4236ca0efb5c6ff82a8e0ff9c974fb6
Pial <pialname394@gmail.com>
2026-04-14 14:47:54 +0600

    rendering friends Overview section from here

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../components/pages/homepage/Banner";
+import FriendsOverview from "./../../components/pages/homepage/FriendsOverview";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <FriendsOverview />
     </div>
   );
 };

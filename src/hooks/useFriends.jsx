5aa240e22ae7119a8a4f3e15d29cad52f94a30b3
Pial <pialname394@gmail.com>
2026-04-14 16:13:50 +0600

    added setTimeOut functionality for checking the loading spinners

--- src/hooks/useFriends.jsx
+++ src/hooks/useFriends.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 
 const useFriends = () => {
   const [friends, setFriends] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("/friends.json");
       const data = await res.json();
-      setFriends(data);
-      setLoading(true);
+      setTimeout(() => {
+        setFriends(data);
+        setLoading(false);
+      }, 2000);
     };
     fetchData();
   }, []);

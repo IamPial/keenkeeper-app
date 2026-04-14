ce1edd1c6996c4893501a0777842d9d6232e7d37
Pial <pialname394@gmail.com>
2026-04-15 01:27:46 +0600

    redesign the btn markup

--- src/components/shared/CardDetailsBtn.jsx
+++ src/components/shared/CardDetailsBtn.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const CardDetailsBtn = ({ imgSrc, text }) => {
   return (
-    <div className=" rounded-lg text-center p-8 space-y-3 shadow-sm ">
-      <img src={imgSrc} alt={text} className="mx-auto w-8" />
-      <h2 className="text-lg text-slate-500 ">{text}</h2>
-    </div>
+    <button className="btn rounded-lg text-center p-15  mt-4 ">
+      <div className="space-y-3  ">
+        <img src={imgSrc} alt={text} className="mx-auto w-8" />
+        <h2 className="text-lg text-slate-500 ">{text}</h2>
+      </div>
+    </button>
   );
 };
 

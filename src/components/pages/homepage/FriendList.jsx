f774f55f19698d258e477a6339acfbd5b4f3674d
Pial <pialname394@gmail.com>
2026-04-14 15:12:50 +0600

    created friend list markup with responsive design

--- src/components/pages/homepage/FriendList.jsx
+++ src/components/pages/homepage/FriendList.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Card from "../../../ui/Card";
 
 const FriendList = () => {
   return (
-    <div>
-      <h2>FriendList</h2>
+    <div className="container mx-auto pb-20 px-2">
+      <div className="space-y-4">
+        <h2 className="font-semibold text-2xl text-slate-800">Your Friends</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <Card></Card>
+        </div>
+      </div>
     </div>
   );
 };

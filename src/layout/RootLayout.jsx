45a9608930344d9d0831458920dc36279dacc719
Pial <pialname394@gmail.com>
2026-04-15 03:38:25 +0600

    added toastContainer into the root components

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/shared/Navbar";
 import Footer from "../components/shared/Footer";
+import { ToastContainer } from "react-toastify";
 
 const RootLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <ToastContainer />
       <Footer />
     </div>
   );

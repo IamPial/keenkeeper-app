d2f23213ca6e37d860b0dc9b37b9f8d094d4f303
Pial <pialname394@gmail.com>
2026-04-14 12:23:58 +0600

    created banner section heading markup

--- src/components/pages/homepage/Banner.jsx
+++ src/components/pages/homepage/Banner.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { FiPlus } from "react-icons/fi";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>Banner Page</h2>
+    <div className="pt-20">
+      <div className="container mx-auto px-2">
+        <div className="space-y-4 text-center">
+          <h2 className="font-bold text-5xl text-slate-900">
+            Friends to keep close in your life
+          </h2>
+          <p className="text-gray-500">
+            Your personal shelf of meaningful connections. Browse, tend, and
+            nurture the <br /> relationships that matter most.
+          </p>
+          <div className="mt-8">
+            <button className="btn bg-[#244D3F] text-white shadow-none">
+              <FiPlus /> Add a Friend
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

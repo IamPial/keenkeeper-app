82d7a7acb77896a1834847d0c75f7c85bb864319
Pial <pialname394@gmail.com>
2026-04-15 03:38:52 +0600

    wrap up the whole projects with context

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import router from "./router/Routes";
 import { RouterProvider } from "react-router";
+import FriendContextProvider from "./context/FriendsContextProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <FriendContextProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </FriendContextProvider>
   </StrictMode>,
 );

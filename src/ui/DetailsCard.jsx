b5354c6f11b06f7d133a5b06c943877c50fd1eb3
Pial <pialname394@gmail.com>
2026-04-15 01:03:12 +0600

    extract all the keys from the expectedObj

--- src/ui/DetailsCard.jsx
+++ src/ui/DetailsCard.jsx
@@ -7,43 +7,79 @@ import FriendsOverviewCard from "./FriendsOverviewCard";
 import callImg from "../assets/call.png";
 import textImg from "../assets/text.png";
 import videoImg from "../assets/video.png";
+import { useParams } from "react-router";
+import CardDetailsBtn from "../components/shared/CardDetailsBtn";
+
+//  {
+//     "id": 12,
+//     "name": "Sophia Martinez",
+//     "picture": "https://i.ibb.co.com/rGDn16G1/shopia.jpg",
+//     "email": "sophia.martinez@example.com",
+//     "days_since_contact": 1,
+//     "status": "on-track",
+//     "tags": ["hobby", "travel"],
+//     "bio": "Met through a language exchange program. Planning future trips together.",
+//     "goal": 30,
+//     "next_due_date": "2026-05-13"
+//   }
 
 const DetailsCard = () => {
   const { friends } = useFriends();
+  const { id } = useParams();
 
-  const days = friends.filter((item) => item.days_since_contact);
-  console.log(days, "hd");
+  const expectedDetails = friends.find((item) => item.id === parseInt(id));
+  if (!expectedDetails) return null;
+  const {
+    name,
+    picture,
+    email,
+    days_since_contact,
+    status,
+    tags,
+    bio,
+    goal,
+    next_due_date,
+  } = expectedDetails;
+
+  //convert date into a date string with separator
+  const date = new Date(next_due_date).toDateString().slice(4).split(" ");
+  const strDate = date[0] + " " + date[1] + " , " + date[2];
 
   return (
     <div className="py-20 container mx-auto px-2">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Details Card Left Section Start */}
         <div className="col-span-full lg:col-span-1  rounded-lg">
-          <div className="text-center shadow-lg rounded-lg p-6">
+          <div className="text-center shadow-md rounded-lg p-6">
             <img
-              src="https://i.ibb.co.com/rGDn16G1/shopia.jpg"
-              alt="Fatima"
+              src={picture}
+              alt={name}
               className="w-20 h-20 rounded-full mb-3 mx-auto"
             />
             <div className="space-y-2">
-              <h2 className="text-xl font-semibold text-slate-900">
-                Sophia Martinez
-              </h2>
-              <div className="flex flex-col justify-center items-center gap-2">
-                <span className="badge  capitalize py-4 text-white rounded-full bg-[#EF4444]">
-                  OverDue
+              <h2 className="text-xl font-semibold text-slate-900">{name}</h2>
+              <div className="flex flex-col justify-center items-center gap-3 mb-4">
+                <span
+                  className={`badge  capitalize py-4 text-white rounded-full font-medium ${status === "almost due" ? "badge bg-[#EFAD44]" : status === "on-track" ? "bg-[#244D3F]" : "bg-[#EF4444]"}`}
+                >
+                  {status}
                 </span>
-                <span className="badge bg-[#CBFADB] text-[#244D3F] py-4 uppercase rounded-full font-medium ">
-                  Work
-                </span>
+
+                <div className="flex  gap-2 justify-center items-center">
+                  {tags.map((item, index) => {
+                    return (
+                      <span
+                        key={index}
+                        className="badge bg-[#CBFADB] text-[#244D3F] py-4 uppercase rounded-full font-medium "
+                      >
+                        {item}
+                      </span>
+                    );
+                  })}
+                </div>
               </div>
-              <p className="italic text-slate-500 font-medium">
-                Met through a language exchange program. Planning future trips
-                together.
-              </p>
-              <p className="text-slate-500 text-sm">
-                sophia.martinez@example.com
-              </p>
+              <p className="italic text-slate-500 font-medium">{bio}</p>
+              <p className="text-slate-500 text-sm">{email}</p>
             </div>
           </div>
           <div className="flex flex-col mt-4 gap-2">
@@ -67,9 +103,12 @@ const DetailsCard = () => {
         <div className=" col-span-full lg:col-span-2 rounded-lg">
           <div className="flex flex-col gap-6">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              <FriendsOverviewCard value="62" text="Days Since Contact" />
-              <FriendsOverviewCard text="Goal (Days)" value="30" />
-              <FriendsOverviewCard text="Next Due" value="Feb 27, 2026" />
+              <FriendsOverviewCard
+                text="Days Since Contact"
+                value={days_since_contact}
+              />
+              <FriendsOverviewCard text="Goal (Days)" value={goal} />
+              <FriendsOverviewCard text="Next Due" value={strDate} />
             </div>
             <div className="shadow-sm p-6 rounded-lg">
               <div className="flex justify-between mb-5">
@@ -79,9 +118,9 @@ const DetailsCard = () => {
                 <button className="btn font-medium text-sm">Edit</button>
               </div>
               <p className="text-lg text-slate-500">
-                Connect Every{" "}
-                <span className="font-bold text-lg text-slate-800">
-                  30 Days
+                Connect Every
+                <span className="ml-1 font-bold text-lg text-slate-800">
+                  {goal} Days
                 </span>
               </p>
             </div>
@@ -90,18 +129,9 @@ const DetailsCard = () => {
                 Quick Check-In
               </h2>
               <div className="grid grid-cols-3 gap-6">
-                <div className=" rounded-lg text-center p-8 space-y-3 shadow-sm ">
-                  <img src={callImg} alt="Call" className="mx-auto w-8" />
-                  <h2 className="text-lg text-slate-500 ">Call</h2>
-                </div>
-                <div className=" rounded-lg text-center p-8 space-y-3 shadow-sm ">
-                  <img src={textImg} alt="Call" className="mx-auto w-8" />
-                  <h2 className="text-lg text-slate-500 ">Text</h2>
-                </div>
-                <div className=" rounded-lg text-center p-8 space-y-3 shadow-sm ">
-                  <img src={videoImg} alt="Call" className="mx-auto w-8" />
-                  <h2 className="text-lg text-slate-500 ">Video</h2>
-                </div>
+                <CardDetailsBtn imgSrc={callImg} text={"Call"} />
+                <CardDetailsBtn imgSrc={textImg} text={"Text"} />
+                <CardDetailsBtn imgSrc={videoImg} text={"Video"} />
               </div>
             </div>
           </div>
